fbdd89e7a29645e518a2fb74d07205a23c83cd0d
SURI <snyeg28842@gmail.com>
2024-04-24 17:12:19 +0900

    feat: 구성 파일

--- src/main/java/com/example/moija_project/config/MongoConfig.java
+++ src/main/java/com/example/moija_project/config/MongoConfig.java
@@ -4,7 +4,10 @@ import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 
 import java.util.Collection;
@@ -12,6 +15,8 @@ import java.util.Collections;
 
 @Configuration
 public class MongoConfig extends AbstractMongoClientConfiguration {
+    @Autowired
+    Environment env;
 
     @Override
     protected String getDatabaseName() {
@@ -20,7 +25,8 @@ public class MongoConfig extends AbstractMongoClientConfiguration {
 
     @Override
     public MongoClient mongoClient() {
-        ConnectionString connectionString = new ConnectionString("mongodb://root:3322@moija-mongodb-1:27017/moija?authSource=admin&authMechanism=SCRAM-SHA-1");
+        //ConnectionString connectionString = new ConnectionString("mongodb://root:3322@moija-mongodb-1:27017/moija?authSource=admin&authMechanism=SCRAM-SHA-1");
+        ConnectionString connectionString = new ConnectionString(env.getProperty("spring.data.mongodb.uri"));
         MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
                 .applyConnectionString(connectionString)
                 .build();
