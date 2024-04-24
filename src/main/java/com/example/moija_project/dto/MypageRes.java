7ce875007802f3e3099acef75c4f8c1fccd9e997
SURI <snyeg28842@gmail.com>
2024-04-24 17:12:01 +0900

    feat: DTO

--- src/main/java/com/example/moija_project/dto/MypageRes.java
+++ src/main/java/com/example/moija_project/dto/MypageRes.java
@@ -1,5 +1,8 @@
 package com.example.moija_project.dto;
 
+import com.example.moija_project.entities.Member;
+import com.example.moija_project.entities.Recruit;
+import com.example.moija_project.entities.Waiting;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
@@ -19,7 +22,38 @@ public class MypageRes {
         @JsonProperty("latest_write")
         Timestamp latestWrite;
 
+
     }
+
+    @Getter@Setter@Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class AskListRes {
+        //리더가 되거나 / 요청자가 되거나
+        MemDto users;
+        String title;
+        @JsonProperty("post_id")
+        Long postId;
+        @JsonProperty("latest_write")
+        Timestamp latestWrite;
+        public static AskListRes from(Waiting waitings) {
+            Recruit recruit = waitings.getRecruit();
+            MemDto memDto = MypageRes.MemDto.builder()
+                    .waitingId(waitings.getWaitingId())
+                    .is_ask(waitings.isAsk())
+                    .nickname(waitings.getUser().getNickname())
+                    .build();
+
+            return AskListRes.builder()
+                    .title(recruit.getTitle())
+                    .postId(recruit.getRecruitId())
+                    .latestWrite(recruit.getLatestWrite())
+                    .users(memDto)
+                    .build();
+        }
+
+    }
+
     @Setter@Builder@Getter@AllArgsConstructor@NoArgsConstructor
     public static class MemDto {
         String nickname;
@@ -29,12 +63,35 @@ public class MypageRes {
 
     @Setter@Builder@Getter@AllArgsConstructor@NoArgsConstructor
     public static class WaitingRes {
+        @JsonProperty("user_id")
+        String userId;
         String nickname;
+        @JsonProperty("reliability_user")
+        Float reliabilityUser;
+        @JsonProperty("profile_url")
+        String profileUrl;
         String gender;
         //프로필사진 추가 나중에...
         String genaration;
         List<QnADTO> qnas;
         boolean is_ask;
 
+    }
+    @Setter@Builder@Getter@AllArgsConstructor@NoArgsConstructor
+    public static class MemListRes {
+        String nickname;
+        @JsonProperty("user_id")
+        String userId;
+        boolean grant;
+
+        public MemListRes(String nickname, String userId) {
+            this.nickname = nickname;
+            this.userId = userId;
+        }
+    }
+    @Setter@Builder@Getter@AllArgsConstructor@NoArgsConstructor
+    public static class AcceptRes {
+        String title;
+        Long postId;
     }
 }
